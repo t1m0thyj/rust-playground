c3eff76da5bb87fe11d3b7cb0ac997b346c65cf9
Timothy Johnson <timothy.johnson@broadcom.com>
2022-07-05 13:05:39 -0400

    Add more test cases
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- stdin-test/test.sh
+++ stdin-test/test.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+set -x
+
+echo "Hello world!" | ./target/debug/stdin-test.exe
+
+cat Cargo.toml | ./target/debug/stdin-test.exe
+
 cat << ___ENDCAT | ./target/debug/stdin-test.exe
 This text was uploaded through standard input on
 `date`
