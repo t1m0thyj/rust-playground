37eea53b490ddc2f0d1d154baa8219e91b42e35b
Timothy Johnson <timothy.johnson@broadcom.com>
2022-07-05 14:01:31 -0400

    Upload a larger test file
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- stdin-test/test.sh
+++ stdin-test/test.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -x
 
+dd if=/dev/urandom of=testfile bs=1024 count=1024
+
 echo "Hello world!" | ./target/debug/stdin-test.exe
 
-cat Cargo.toml | ./target/debug/stdin-test.exe
+cat testfile | ./target/debug/stdin-test.exe
 
 cat << ___ENDCAT | ./target/debug/stdin-test.exe
 This text was uploaded through standard input on
 `date`
 ___ENDCAT
+
+rm testfile
